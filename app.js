9f2c78d5e824c88f84e58cf1fe2183f6517ada7e
anhdo <dinhanhdo95@gmail.com>
2019-10-19 17:48:50 +0700

    Setup mongdb connection

--- app.js
+++ app.js
@@ -3,17 +3,10 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const { rootDir } = require('./util/path')
-const sequelize = require('./util/database')
-
-const Product = require('./models/product')
-const User = require('./models/user')
-const Cart = require('./models/cart')
-const CartItem = require('./models/cart-item')
-const Order = require('./models/order')
-const OrderItem = require('./models/order-item')
+const { mongoConnect } = require('./util/database')
 
 const adminRoutes = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
+// const shopRoutes = require('./routes/shop')
 const { get404 } = require('./controllers/error')
 
 const app = express()
@@ -25,53 +18,20 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static(path.join(rootDir, 'public')))
 
 app.use((req, res, next) => {
-  User.findByPk(1)
-    .then(user => {
-      req.user = user
-      next()
-    })
-    .catch(console.error)
+  // User.findByPk(1)
+  //   .then(user => {
+  //     req.user = user
+  //     next()
+  //   })
+  //   .catch(console.error)
+  next()
 })
 
 app.use('/admin', adminRoutes)
-app.use(shopRoutes)
+// app.use(shopRoutes)
 
 app.use(get404)
 
-Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' })
-User.hasMany(Product)
-User.hasOne(Cart)
-Cart.belongsTo(User)
-Cart.belongsToMany(Product, { through: CartItem })
-Product.belongsToMany(Cart, { through: CartItem })
-Order.belongsTo(User)
-User.hasMany(Order)
-Order.belongsToMany(Product, { through: OrderItem })
-
-const getUser = (user) => {
-  return user || User.create({
-    name: "Richard",
-    email: "admin@gmail.com"
-  })
-}
-
-const getCart = (user) => {
-  return user
-    .getCart()
-    .then(cart => {
-      return cart || user.createCart({
-        userId: user.id
-      })
-    })
-    .catch(console.error)
-}
-
-sequelize
-  // .sync({ force: true })
-  .sync()
-  .then(() => User.findByPk(1))
-  .then(getUser)
-  .then(getCart)
-  .then(() => app.listen(9000))
-  .catch(console.error);
-
+mongoConnect(() => {
+  app.listen(3000)
+})
--- util/database.js
+++ util/database.js
@@ -1,8 +1,28 @@
-const Sequelize = require('sequelize')
+const { MongoClient } = require('mongodb')
 
-const sequelize = new Sequelize('node-complete', 'nodecomplete', 'nodecomplete', {
-  host: 'localhost',
-  dialect: 'mysql'
-})
+let _db
 
-module.exports = sequelize;
+const mongoConnect = (cb) => {
+  const uri = 'mongodb+srv://admin:admin@node-complete-1dj8a.mongodb.net/shop?retryWrites=true&w=majority'
+  const client = new MongoClient(uri, { useUnifiedTopology: true })
+
+  client.connect(err => {
+    if (err) {
+      throw err
+    }
+
+    console.log('Connected!');
+    _db = client.db()
+  })
+
+  cb()
+}
+
+const getDb = () => {
+  if (_db) return _db
+  throw 'No database found!'
+}
+
+module.exports.mongoConnect = mongoConnect
+module.exports.getDb = getDb
+
