e3d323837ae4972be604c28b146d3487a2c8b2aa
anhdo <dinhanhdo95@gmail.com>
2019-10-24 23:42:53 +0700

    Revert "Add sign-up and authenticating middleware"
    This reverts commit eff3126e0ec42b3ab6cddb309ae6ac99f1b6ab2a.

--- app.js
+++ app.js
@@ -56,7 +56,19 @@ app.use(errorController.get404);
 
 mongoose
   .connect(MONGODB_URI)
-  .then(() => {
+  .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Max',
+          email: 'max@test.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });
     app.listen(3000);
   })
   .catch(err => {
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,74 +1,39 @@
-const bcrypt = require("bcryptjs");
-const User = require("../models/user");
+const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
-  res.render("auth/login", {
-    path: "/login",
-    pageTitle: "Login",
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
     isAuthenticated: false
   });
 };
 
 exports.getSignup = (req, res, next) => {
-  res.render("auth/signup", {
-    path: "/signup",
-    pageTitle: "Signup",
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
     isAuthenticated: false
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  const { email, password } = req.body;
-
-  User.findOne({ email })
+  User.findById('5bab316ce0a7c75f783cb8a8')
     .then(user => {
-      if (!user) {
-        res.redirect("/login");
-      }
-
-      bcrypt.compare(password, user.password)
-        .then(doMatch => {
-          if (doMatch) {
-            req.session.isLoggedIn = true;
-            req.session.user = user;
-            return req.session.save(err => {
-              console.log(err);
-              res.redirect("/");
-            });
-          }
-          res.redirect("/login")
-        })
-        .catch(console.error)
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      req.session.save(err => {
+        console.log(err);
+        res.redirect('/');
+      });
     })
-    .catch(err => {
-      console.log(err)
-      res.redirect("/login");
-    });
+    .catch(err => console.log(err));
 };
 
-exports.postSignup = (req, res, next) => {
-  const { email, password, confirmPassword } = req.body;
-
-  User.findOne({ email }).then(userDoc => {
-    if (userDoc) {
-      return res.redirect("/signup");
-    }
-
-    return bcrypt
-      .hash(password, 12)
-      .then(hashedPassword => {
-        return new User({
-          email,
-          password: hashedPassword
-        }).save();
-      })
-      .then(() => res.redirect("/login"));
-  });
-};
+exports.postSignup = (req, res, next) => {};
 
 exports.postLogout = (req, res, next) => {
   req.session.destroy(err => {
     console.log(err);
-    res.redirect("/");
+    res.redirect('/');
   });
 };
