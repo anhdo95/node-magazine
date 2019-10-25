ffe6f977e7968418ac4876da1df165da59b64540
anhdo <dinhanhdo95@gmail.com>
2019-10-25 22:17:32 +0700

    Add reset password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,20 +1,28 @@
 const bcrypt = require("bcryptjs");
+const crypto = require("crypto");
 const User = require("../models/user");
-const nodemailer = require('nodemailer')
-const sgTransport = require('nodemailer-sendgrid-transport')
+const nodemailer = require("nodemailer");
+const sgTransport = require("nodemailer-sendgrid-transport");
+// const { SENDGRID_API_KEY }  = require('../util/constant');
 
-const mailer = nodemailer.createTransport(sgTransport({
-  auth: {
-    api_key: 'YOUR_SENDGRID_API_KEY'
-  }
-}));
+const hoursToMilliseconds = (hours) => {
+  return hours * 60 * 60 * 1000
+}
+
+const mailer = nodemailer.createTransport(
+  sgTransport({
+    auth: {
+      // api_key: SENDGRID_API_KEY
+    }
+  })
+);
 
 exports.getLogin = (req, res, next) => {
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
     isAuthenticated: false,
-    errorMessage: req.flash('error')
+    errorMessage: req.flash("error")
   });
 };
 
@@ -22,7 +30,7 @@ exports.getSignup = (req, res, next) => {
   res.render("auth/signup", {
     path: "/signup",
     pageTitle: "Signup",
-    errorMessage: req.flash('error')
+    errorMessage: req.flash("error")
   });
 };
 
@@ -32,11 +40,12 @@ exports.postLogin = (req, res, next) => {
   User.findOne({ email })
     .then(user => {
       if (!user) {
-        req.flash('error', 'Email and password are invalid')
+        req.flash("error", "Email and password are invalid");
         res.redirect("/login");
       }
 
-      bcrypt.compare(password, user.password)
+      bcrypt
+        .compare(password, user.password)
         .then(doMatch => {
           if (doMatch) {
             req.session.isLoggedIn = true;
@@ -46,12 +55,12 @@ exports.postLogin = (req, res, next) => {
               res.redirect("/");
             });
           }
-          res.redirect("/login")
+          res.redirect("/login");
         })
-        .catch(console.error)
+        .catch(console.error);
     })
     .catch(err => {
-      console.log(err)
+      console.log(err);
       res.redirect("/login");
     });
 };
@@ -61,7 +70,7 @@ exports.postSignup = (req, res, next) => {
 
   User.findOne({ email }).then(userDoc => {
     if (userDoc) {
-      req.flash('error', 'Email exists already, please pick a different one.')
+      req.flash("error", "Email exists already, please pick a different one.");
       return res.redirect("/signup");
     }
 
@@ -74,18 +83,18 @@ exports.postSignup = (req, res, next) => {
         }).save();
       })
       .then(() => {
-        res.redirect("/login")
+        res.redirect("/login");
 
         const options = {
           to: email,
-          from: 'shop@node-complete.com', // From your configured senders
-          subject: 'Signup succeeded!',
-          html: '<h1>You successfully signed up!</h1>'
+          from: "shop@node-complete.com", // From your configured senders
+          subject: "Signup succeeded!",
+          html: "<h1>You successfully signed up!</h1>"
         };
 
-        return mailer.sendMail(options)
+        return mailer.sendMail(options);
       })
-      .catch(console.error)
+      .catch(console.error);
   });
 };
 
@@ -95,3 +104,108 @@ exports.postLogout = (req, res, next) => {
     res.redirect("/");
   });
 };
+
+exports.getReset = (req, res, next) => {
+  res.render("auth/reset", {
+    path: "/reset",
+    pageTitle: "Reset Password",
+    errorMessage: req.flash("error")
+  });
+};
+
+exports.postReset = (req, res, next) => {
+  const { email } = req.body;
+
+  crypto.randomBytes(32, (error, buffer) => {
+    if (error) {
+      console.error(error);
+      return res.redirect("/reset");
+    }
+
+    const token = buffer.toString('hex');
+
+    User.findOne({ email })
+      .then(user => {
+        if (!user) {
+          req.flash("error", "No account with that email found");
+          return res.redirect("/reset");
+        }
+
+        user.resetToken = token;
+        user.resetTokenExpiration = Date.now() + hoursToMilliseconds(2)
+        return user.save();
+      })
+      .then(() => {
+        res.redirect("/");
+
+        const options = {
+          to: email,
+          from: "shop@node-complete.com",
+          subject: "Reset Password",
+          html: `
+            <p>You requested a password reset</p>
+            <p>
+              Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password
+            </p>
+          `
+        };
+
+        console.log('options :', options);
+
+        return mailer.sendMail(options);
+      })
+      .catch(console.error);
+  });
+};
+
+exports.getNewPassword = (req, res, next) => {
+  const { token: resetToken } = req.params
+
+  User.findOne({
+    resetToken,
+    resetTokenExpiration: { $gt: Date.now() }
+  })
+  .then(user => {
+    if (!user) {
+      return res.redirect("/login");
+    }
+
+    res.render("auth/new-password", {
+      path: "/new-password",
+      pageTitle: "Update A New Password",
+      userId: user._id,
+      passwordToken: resetToken
+    });
+  })
+}
+
+exports.postNewPassword = (req, res, next) => {
+  const { userId, passwordToken, newPassword } = req.body;
+  let resetUser
+
+  User.findOne({
+    _id: userId,
+    resetToken: passwordToken,
+    resetTokenExpiration: { $gt: Date.now() }
+  })
+    .then(user => {
+      if (!user) {
+        return res.redirect("/login");
+      }
+
+      resetUser = user
+      return bcrypt.hash(newPassword, 12)
+    })
+    .then(hashPassword => {
+      resetUser.password = hashPassword
+      resetUser.resetToken = undefined
+      resetUser.resetTokenExpiration = undefined
+      return resetUser.save()
+    })
+    .then((result) => {
+      if (result) {
+        return res.redirect("/login");
+      }
+    })
+    .catch(console.error)
+}
--- util/constant.js
+++ util/constant.js
@@ -1,4 +1,3 @@
 module.exports = {
   MONGODB_URI: 'mongodb+srv://admin:admin@node-complete-1dj8a.mongodb.net/shop?retryWrites=true&w=majority',
-  SENDGRID_API_KEY: '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'
 }
